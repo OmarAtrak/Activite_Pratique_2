7e7086a1af5bb6b515337762c6968772aef180bc
Omar Atrak <omar.atrak7@gmail.com>
2024-03-10 13:11:32 +0000

    upload project

---
+++ src/test/java/enset/ma/app/ActivitePratique2ApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.ma.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitePratique2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
